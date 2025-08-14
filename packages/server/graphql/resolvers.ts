2ac5884247f305ec05adaf3862ef46e83104d857
Naoki427 <156777871+Naoki427@users.noreply.github.com>
2025-08-15 01:49:14 +0900

    fix for ci error

--- packages/server/graphql/resolvers.ts
+++ packages/server/graphql/resolvers.ts
@@ -1,10 +1,12 @@
 import { mergeResolvers } from "@graphql-tools/merge";
+import baseResolver   from "./base.resolver.js";
 import scalarResolver from "./scalars/scalar.resolver.js";
 import userResolver from "./users/user.resolver.js";
 import itemResolver from "./items/item.resolver.js";
 
 export const resolvers = mergeResolvers([
-  scalarResolver,
-  userResolver,
-  itemResolver,
+    baseResolver,
+    scalarResolver,
+    userResolver,
+    itemResolver,
 ]);
